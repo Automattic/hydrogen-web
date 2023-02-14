a578409c747253f656a52b7390b462b7d6ad7d2a
Paulo Pinto <paulo.pinto@automattic.com>
2023-02-14 16:02:00 +0000

    Make SessionPool capable of holding clients for multiple sessions

--- src/pool/SessionPool.ts
+++ src/pool/SessionPool.ts
@@ -2,19 +2,33 @@ import {Platform} from "../platform/web/Platform";
 import {FeatureSet} from "../features";
 import {Client} from "../matrix/Client";
 
+type SessionId = string;
+
 export class SessionPool {
-    private readonly _client: Client;
+    private readonly _clients: Map<SessionId, Client> = new Map;
+    private readonly _platform: Platform;
+    private readonly _features: FeatureSet;
 
     constructor(platform: Platform, features: FeatureSet) {
-        this._client = new Client(platform, features);
+        this._platform = platform;
+        this._features = features;
     }
 
-    // TODO REFACTOR: Remove this method since client should not be exposed.
-    client(sessionId: string): Client {
-        return this._client;
+    // TODO REFACTOR: Make this method private since client should not be exposed.
+    client(sessionId: SessionId): Client {
+        if (!this._clients.has(sessionId)) {
+            throw `Session with id ${sessionId} not found in pool`;
+        }
+
+        return this._clients.get(sessionId);
     }
 
-    startWithExistingSession(sessionId: string) {
-        this._client.startWithExistingSession(sessionId);
+    startWithExistingSession(sessionId: SessionId) {
+        if (this._clients.has(sessionId)) {
+            throw `Session with id ${sessionId} is already in pool`;
+        }
+        const client = new Client(this._platform, this._features);
+        this._clients.set(sessionId, client);
+        client.startWithExistingSession(sessionId);
     }
 }
