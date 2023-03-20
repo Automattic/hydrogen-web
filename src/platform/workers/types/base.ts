e6d1ea22931b20ebc2eb80b3ddd1b75445991043
Paulo Pinto <paulo.pinto@automattic.com>
2023-03-20 15:58:04 +0000

    Add EventBus

--- src/platform/workers/types/base.ts
+++ src/platform/workers/types/base.ts
@@ -17,6 +17,19 @@ export interface Response {
     data?: object;
 }
 
+export type EventId = string;
+export type EventType = string;
+
+export interface Event {
+    id: EventId;
+    type: EventType;
+    data?: object;
+}
+
+export function makeEventId(): EventId {
+    return makeId() as EventId;
+}
+
 function makeId(): string {
     return (Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)).toString();
 }
