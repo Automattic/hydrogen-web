d30711dc7c84d7104de01e389b0f20f08aaa0361
Paulo Pinto <paulo.pinto@automattic.com>
2023-03-22 14:39:18 +0000

    Instantiate SessionFactory

--- src/platform/workers/sync/SyncWorker.ts
+++ src/platform/workers/sync/SyncWorker.ts
@@ -6,23 +6,33 @@ import assetPaths from "../../web/sdk/paths/vite";
 import {Reconnector} from "../../../matrix/net/Reconnector";
 import {ExponentialRetryDelay} from "../../../matrix/net/ExponentialRetryDelay";
 import {OnlineStatus} from "../../web/dom/OnlineStatus";
+import {SessionFactory} from "../../../matrix/SessionFactory";
+import {FeatureSet} from "../../../features";
 
 export class SyncWorker extends SharedWorker {
     private readonly _eventBus: BroadcastChannel;
     private readonly _platform: SyncPlatform;
+    private readonly _features: FeatureSet;
     private readonly _onlineStatus: OnlineStatus;
     private readonly _reconnector: Reconnector;
+    private readonly _sessionFactory: SessionFactory;
 
     constructor() {
         super();
         this._eventBus = new BroadcastChannel(this.name);
         this._platform = new SyncPlatform({assetPaths});
+        this._features = new FeatureSet;
         this._onlineStatus = new OnlineStatus;
         this._reconnector = new Reconnector({
             onlineStatus: this._onlineStatus,
             retryDelay: new ExponentialRetryDelay(this._platform.clock.createTimeout),
             createMeasure: this._platform.clock.createMeasure
         });
+        this._sessionFactory = new SessionFactory({
+            platform: this._platform,
+            features: this._features,
+            reconnector: this._reconnector,
+        })
 
         this.setHandler(SyncRequestType.StartSync, this.startSync.bind(this));
     }
